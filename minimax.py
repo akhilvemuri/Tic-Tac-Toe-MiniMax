89fa8ade02db5ce79df1ddd2b7c0222902edd087
akhilvemuri <akhilvemuri@gmail.com>
2020-12-30 19:07:55 -0800

    Added win, lose, and draw features. Also cleaned up some of the code

--- minimax.py
+++ minimax.py
@@ -1,6 +1,10 @@
+import random
+import copy
+
+
 board = [[0]*3 for i in range(3)]
 HUMAN = -1
-COMP = +1
+COMP = 1
 
 
 def convert_board_to_input():
@@ -12,7 +16,7 @@ def display_board(inputs):
     
     mappings = {
         -1: 'X',
-        +1: 'O',
+         1: 'O',
          0: '-'
     }
     
@@ -47,9 +51,9 @@ def win(player, state):
     return [player]*3 in win_states
     
 def is_game_over(state):
-    return not empty_cells(state) or win(HUMAN, state) or win(COMP, state)
+    return not empty_spaces(state) or win(HUMAN, state) or win(COMP, state)
 
-def empty_cells(state):
+def empty_spaces(state):
     empty = []
     for i in range(len(board)):
         for j in range(len(board)):
@@ -59,7 +63,7 @@ def empty_cells(state):
     return empty
 
 def make_move(player, pos_x, pos_y):
-    if [pos_x, pos_y] in empty_cells(board):
+    if [pos_x, pos_y] in empty_spaces(board):
         board[pos_x][pos_y] = player
         return True
     else:
@@ -76,26 +80,52 @@ def human_turn():
         7: [2, 0], 8: [2, 1], 9: [2, 2],
     }
     
-    while pos < 1 or pos > 9:
+    while not (0 < pos < 10):
         try:
             pos = int(input('Your Turn (X)! Enter a coordinate (1..9): '))
             coords = moves[pos]
-            moved = make_move(HUMAN, coords[0], coords[1])
+            successful_move = make_move(HUMAN, coords[0], coords[1])
 
-            if not moved:
+            if not successful_move:
                 print('\nBad input. Please try again.')
                 pos = -1
-        except (EOFError, KeyboardInterrupt):
-            print('Bye')
-            exit()
         except (KeyError, ValueError, TypeError):
             print('\nBad input. Please try again.')
-            
+        except (EOFError, KeyboardInterrupt):
+            exit()
+
+
+def minimax(state, depth, player):
+    best_score = [-1, -1, float('-inf') if player == COMP else float('inf')]
+
+    if depth == 0 or is_game_over(state):
+        if win(COMP, state):
+            score = 1
+        elif win(HUMAN, state):
+            score = -1
+        else:
+            score = 0
+        return [-1, -1, score]
+
+    for pos_x, pos_y in empty_spaces(state):
+        state[pos_x][pos_y] = player
+        curr_score = minimax(state, depth - 1, -player)
+        
+        state[pos_x][pos_y] = 0
+        curr_score[0], curr_score[1] = pos_x, pos_y
+
+        if player == COMP:
+            best_score = max(best_score, curr_score, key=lambda x: x[2])
+        else:
+            best_score = min(best_score, curr_score, key=lambda x: x[2])
+
+    return best_score
+
 def comp_turn():
     if is_game_over(board):
         return
     
-    depth = len(empty_cells(board))
+    depth = len(empty_spaces(board))
     if depth == 9:
         pos_x, pos_y = random.choice([0, 1, 2]), random.choice([0, 1, 2])
     else:
@@ -103,41 +133,7 @@ def comp_turn():
         pos_x, pos_y = move[0], move[1]
     
     print('Computer Turn (O).')
-    make_move(COMP, pos_x, pos_y)
-    
-    
-def minimax(state, depth, player):
-    if player == COMP:
-        best = [-1, -1, float('-inf')]
-    else:
-        best = [-1, -1, float('inf')]
-
-    if depth == 0 or is_game_over(state):
-        if win(COMP, state):
-            score = +1
-        elif win(HUMAN, state):
-            score = -1
-        else:
-            score = 0
-        return [-1, -1, score]
-
-    for cell in empty_cells(state):
-        pos_x, pos_y = cell[0], cell[1]
-        state[pos_x][pos_y] = player
-        
-        score = minimax(state, depth - 1, -player)
-        
-        state[pos_x][pos_y] = 0
-        score[0], score[1] = pos_x, pos_y
-
-        if player == COMP:
-            if score[2] > best[2]:
-                best = score  # max value
-        else:
-            if score[2] < best[2]:
-                best = score  # min value
-
-    return best    
+    make_move(COMP, pos_x, pos_y)    
 
 
 
@@ -152,9 +148,18 @@ def main():
     while not is_game_over(board):
         human_turn()
         display_board(convert_board_to_input())
-        
+
         comp_turn()
         display_board(convert_board_to_input())
+
+    if win(HUMAN, board):
+        print("Congratualations, you've beaten the unbeatable AI! (what hacks did you use?)")
+    elif win(COMP, board):
+        print("Game Over! You Lost.")
+    else:
+        print("Draw!")
+
+    exit()
 
 if __name__ == "__main__":
     main()
