76b548268975d956ecc70650379b5deb02730fe0
akhilvemuri <akhilvemuri@gmail.com>
2020-12-30 21:48:50 -0800

    Commented confusing parts

--- minimax.py
+++ minimax.py
@@ -1,27 +1,23 @@
-import random
-import copy
+board = [[0]*3 for i in range(3)]   
+HUMAN = -1  # The human is the minimizing player
+COMP = 1    # The AI is the maximizing player
 
-
-board = [[0]*3 for i in range(3)]
-HUMAN = -1
-COMP = 1
-
-
-def convert_board_to_input():
-    return [board[0][0], board[0][1], board[0][2],
-            board[1][0], board[1][1], board[1][2],
-            board[2][0], board[2][1], board[2][2]]
 
 def display_board(inputs):
-    
-    mappings = {
+    """
+    Prints out tic-tac-toe board based on inputs (usually board state values)
+
+    :param inputs: array of values to print on the board
+    :return: None
+    """
+    mappings = {    # Mapping of player values to symbols
         -1: 'X',
          1: 'O',
          0: '-'
     }
     
     converted = [mappings[x] for x in inputs if x in mappings]
-    if converted:
+    if converted:   # Checks if the inputs are the board state values
         inputs = converted
     inputs = [' {0} '.format(x) for x in inputs]
     
@@ -36,7 +32,23 @@ def display_board(inputs):
                inputs[6], inputs[7], inputs[8]))
 
 
+def convert_board_to_input():
+    """
+    Helper function that converts board to inputs for display_board
+    """
+    return [board[0][0], board[0][1], board[0][2],
+            board[1][0], board[1][1], board[1][2],
+            board[2][0], board[2][1], board[2][2]]
+
+
 def win(player, state):
+    """
+    Has a player has won the game
+
+    :param player: HUMAN or COMP
+    :param state: the current board
+    :return: bool
+    """
     win_states = [
         [state[0][0], state[0][1], state[0][2]],
         [state[1][0], state[1][1], state[1][2]],
@@ -51,9 +63,19 @@ def win(player, state):
     return [player]*3 in win_states
     
 def is_game_over(state):
+    """
+    Any more spaces or has someone won
+
+    :return: bool
+    """
     return not empty_spaces(state) or win(HUMAN, state) or win(COMP, state)
 
 def empty_spaces(state):
+    """
+    Finds all the empty cells on the board
+
+    :return: array of [x, y] indices that have 0 value
+    """
     empty = []
     for i in range(len(board)):
         for j in range(len(board)):
@@ -63,6 +85,13 @@ def empty_spaces(state):
     return empty
 
 def make_move(player, pos_x, pos_y):
+    """
+    Updates board for given player move
+
+    :param pos_x: row index of move (int)
+    :param pos_y: column index of move (int)
+    :return: bool of whether move was successfully made
+    """
     if [pos_x, pos_y] in empty_spaces(board):
         board[pos_x][pos_y] = player
         return True
@@ -70,6 +99,9 @@ def make_move(player, pos_x, pos_y):
         return False
 
 def human_turn():
+    """
+    Asks for human player's move and updates board state accordingly
+    """
     if is_game_over(board):
         return
     
@@ -80,59 +112,70 @@ def human_turn():
         7: [2, 0], 8: [2, 1], 9: [2, 2],
     }
     
-    while not (0 < pos < 10):
+    while not (0 < pos < 10):   # Checks for any invalid inputs
         try:
             pos = int(input('Your Turn (X)! Enter a coordinate (1..9): '))
             coords = moves[pos]
             successful_move = make_move(HUMAN, coords[0], coords[1])
 
             if not successful_move:
-                print('\nBad input. Please try again.')
+                print('\nInvalid input. Please try again.')
                 pos = -1
         except (KeyError, ValueError, TypeError):
-            print('\nBad input. Please try again.')
+            print('\nInvalid input. Please try again.')
         except (EOFError, KeyboardInterrupt):
             exit()
 
 
 def minimax(state, depth, player):
-    best_score = [-1, -1, float('-inf') if player == COMP else float('inf')]
+    """
+    Runs through decision tree using recursion and finds best
+    possible move for the AI to play
 
-    if depth == 0 or is_game_over(state):
+    :return: array of [x_pos, y_pos, best_eval]
+    """
+    if depth == 0 or is_game_over(state):       # Base case
         if win(COMP, state):
-            score = 1
+            static_eval = 1
         elif win(HUMAN, state):
-            score = -1
+            static_eval = -1
         else:
-            score = 0
-        return [-1, -1, score]
+            static_eval = 0
+        # Returns static evaluation (-1 --> human win, +1 --> comp win, 0 --> draw)
+        # of current position
+        return [-1, -1, static_eval]
+
+    if player == COMP:
+        best_eval = [-1, -1, float('-inf')]     # Min initial for maximizing player
+    else:
+        best_eval = [-1, -1, float('inf')]      # Max initial for minimizing player
 
     for pos_x, pos_y in empty_spaces(state):
         state[pos_x][pos_y] = player
-        curr_score = minimax(state, depth - 1, -player)
+        curr_eval = minimax(state, depth - 1, -player)
         
-        state[pos_x][pos_y] = 0
-        curr_score[0], curr_score[1] = pos_x, pos_y
+        state[pos_x][pos_y] = 0     # Reset to empty state
+        curr_eval[0], curr_eval[1] = pos_x, pos_y       # Add "best" pos_x & pos_y for AI's move
 
         if player == COMP:
-            best_score = max(best_score, curr_score, key=lambda x: x[2])
+            best_eval = max(best_eval, curr_eval, key=lambda x: x[2])
         else:
-            best_score = min(best_score, curr_score, key=lambda x: x[2])
+            best_eval = min(best_eval, curr_eval, key=lambda x: x[2])
 
-    return best_score
+    return best_eval
+
 
 def comp_turn():
+    """
+    Plays AI's move using "best" pos_x & pos_y from minimax
+    """
     if is_game_over(board):
         return
     
-    depth = len(empty_spaces(board))
-    if depth == 9:
-        pos_x, pos_y = random.choice([0, 1, 2]), random.choice([0, 1, 2])
-    else:
-        move = minimax(board, depth, COMP)
-        pos_x, pos_y = move[0], move[1]
+    move = minimax(board, len(empty_spaces(board)), COMP)
+    pos_x, pos_y = move[0], move[1]
     
-    print('Computer Turn (O).')
+    print('Computer Turn (O):')
     make_move(COMP, pos_x, pos_y)    
 
 
@@ -153,7 +196,7 @@ def main():
         display_board(convert_board_to_input())
 
     if win(HUMAN, board):
-        print("Congratualations, you've beaten the unbeatable AI! (what hacks did you use?)")
+        print("Congratualations, you've somehow beaten the unbeatable AI!")
     elif win(COMP, board):
         print("Game Over! You Lost.")
     else:
